c31498a7d801ffeebbf11ece41907fd6ac53ce9a
vkuznetsoff <ss_slavich@mail.ru>
2024-10-21 09:01:13 +0300

    Create style and set js Data

---
+++ task1.js
@@ -0,0 +1,8 @@
+const currentDate = new Date();
+console.log(currentDate);
+
+const targetDate = new Date('YYYY-MM-DDTHH:MM:SS'); // Replace with target
+console.log(targetDate);
+
+const difference = targetDate - currentDate;
+console.log(difference);
